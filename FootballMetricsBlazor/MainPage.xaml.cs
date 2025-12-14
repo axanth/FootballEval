5cd6fc7beed37dc9a74c88218c019a15e1d24007
Alexandros Xanthoudakis <axanthoudakis@gmail.com>
2025-12-14 13:36:14 +0200

    feat: Add MAUI Blazor Hybrid client

---
+++ FootballMetricsBlazor/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace FootballMetricsBlazor;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ FootballMetricsBlazor/Models/Metric.cs
@@ -0,0 +1,9 @@
+namespace FootballMetricsBlazor.Models;
+
+public class Metric
+{
+    public string Id { get; set; } = "";
+    public string Key { get; set; } = "";
+    public string Label { get; set; } = "";
+    public string Category { get; set; } = "";
+}
